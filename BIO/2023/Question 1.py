08aba5eef21d8e886d14b01c5cd142d4ae97a520
Logs-Git <lorenzodavidfrescobaldi@gmail.com>
2023-01-05 08:57:22 +0000

    Question 1 polished

--- BIO/2023/Question 1.py
+++ BIO/2023/Question 1.py
@@ -1,7 +1,8 @@
 FIBONACCI = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657,
              46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269]
 
-def Zeckendorf(inputN):
+
+def zeckendorf(inputN):
     tempN = inputN
     upperLimit = len(FIBONACCI)
     correctNumbers = []
@@ -24,5 +25,5 @@ def Zeckendorf(inputN):
 
 if __name__ == "__main__":
 
-    print("Lorenzo Frescobaldi - Brighton College \n\n")
+    print("Lorenzo Frescobaldi - Brighton College \n")
     print(Zeckendorf(int(input("> "))))
